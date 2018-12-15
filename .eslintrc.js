d62fdae4070eab6d6c55f06ae47133accf52f509
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-16 01:09:11 +0530

    integrate react

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,22 @@ module.exports = {
     commonjs: true,
     es6: true
   },
-  extends: 'eslint:recommended',
+  extends: ['airbnb'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 2018,
-    module: true
+    sourceType: 'module'
   },
   plugins: ['react'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always']
+    semi: ['error', 'always'],
+    'react/jsx-filename-extension': 0,
+    'comma-dangle': ['error', 'always-multiline']
   }
 };
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <>
+    <p>Welcome !!!</p>
+    <p>Hello</p>
+  </>
+);
+export default App;
