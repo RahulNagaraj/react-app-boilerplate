abb1b8f1e8bbf5604498d34f8f0038fdf4d15c98
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-16 03:29:39 +0530

    configure prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,31 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es6: true
+    es6: true,
   },
-  extends: ['airbnb'],
+  extends: ['airbnb', 'plugin:prettier/recommended'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'react/jsx-filename-extension': 0,
-    'comma-dangle': ['error', 'always-multiline']
-  }
+    'comma-dangle': ['error', 'always-multiline'],
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,13 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
+  arrowParens: 'avoid',
   bracketSpacing: true,
   endOfLine: 'lf',
-  jsxBracketSameLine: true,
+  jsxBracketSameLine: false,
   jsxSingleQuote: false,
   singleQuote: true,
   semi: true,
   tabWidth: 2,
   trailingComma: 'es5',
-  useTabs: false
+  useTabs: false,
 };
