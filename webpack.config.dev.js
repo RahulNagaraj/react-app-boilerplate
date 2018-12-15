7419497169563f267a7f6ae1ad62ee0c92eee3b6
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-15 15:19:11 +0530

    setup babel

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -17,6 +17,7 @@ module.exports = {
   mode: 'development',
   module: {
     rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
         test: /\.css$/,
         use: [
