a9c86c349c5ce43ac5ceaba4fa47879f26e46510
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-15 20:54:08 +0530

    integrate weback and babel + run dev server

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    {
-      targets: {
-        browsers: ['last 2 versions', 'ie >= 10']
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['last 2 versions', 'ie >= 10']
+        }
       }
-    }
+    ],
+    '@babel/preset-react'
   ]
 };
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,21 +3,33 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Simplifies creation of HTML files to serve your webpack bundles
 const CleanWebpackPlugin = require('clean-webpack-plugin'); // Plugin to remove your build folder(s) before building
 
+// Webpack uses `publicPath` to determine where the app is being served from.
+// In development, we always serve from the root. This makes config easier.
+const publicPath = '/';
+// `publicUrl` is just like `publicPath`, but we will provide it to our app
+// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
+const publicUrl = '';
+// Get environment variables to inject into our app.
+
 module.exports = {
+  entry: path.join(__dirname, 'src/index.js'), // Entry point to the app,
+  output: {
+    path: path.join(__dirname, 'build/js'), // Output to /build
+    filename: '[name].bundle.js', // Output filename - index.bundle.js
+    publicPath: publicPath // This is the URL that app is served from. We use "/" in development.
+  },
   devServer: {
-    contentBase: path.join(__dirname, 'src'), // Serve the src folder
+    contentBase: './build', // Serve the src folder
     historyApiFallback: true, // index.html to be served incase of 404
     hot: true, // Hot Module Replacement
     port: 8080 // Port to run on
   },
-  devTool: 'cheap-eval-source-map', // Controls if and how source maps are generated
-  entry: {
-    app: './src/index.js' // Entry point to the app
-  },
+  devtool: 'cheap-eval-source-map', // Controls if and how source maps are generated
   mode: 'development',
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
         test: /\.css$/,
         use: [
@@ -35,10 +47,10 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(['build']),
     new HtmlWebpackPlugin({
       inject: 'body', // Inject assets into the body
-      template: './src/index.html' // Template to render
+      template: path.join(__dirname, 'public/index.html') // Template to render
     }),
     new webpack.HotModuleReplacementPlugin()
   ],
@@ -50,9 +62,5 @@ module.exports = {
     modules: true, // Add built modules information
     maxModules: 0, // Set the maximum number of modules to be shown
     warnings: true // Add warnings
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'), // Output to /dist
-    filename: '[name].bundle.js' // Output filename - index.bundle.js
   }
 };
